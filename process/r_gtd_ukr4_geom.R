d14980b9bd14533fb275b553f17b5c122e07ee50
Noelle Chubb <noellechubb@gmail.com>
2021-12-27 22:35:55 -0600

    cleaning

--- process/r_gtd_ukr4_geom.R
+++ process/r_gtd_ukr4_geom.R
@@ -4,20 +4,19 @@ library(sf)
 library(sp)
 library(dplyr)
 
-
+## Set options to eight for maximum geometric accuracy:
 options(digits = 8)
 
+## Read the file:
 ukr_m <- fromJSON(txt = "./data/merge_output/extr_all.json")
+
+## Set the 'gtd_num' variable as factor:
 ukr_m$gtd_num <- as.factor(ukr_m$gtd_num)
 
+## Convert to numeric:
 ukr_m$lat <- as.numeric(ukr_m$lat)
 ukr_m$lon <- as.numeric(ukr_m$lon)
 
-ukr_m <- unique(ukr_m)
 
-##
-## somehow transferred to SFC...?
-##
-
-
-
+## convert to spatial feature:
+ukr_m2 <- st_as_sf(ukr_m, coords = c("lat", "lon"), crs = 4326)
